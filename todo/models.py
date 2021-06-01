40fc8249c0006a8386f9e28d7d6c9b1f7e196404
flashtyler <84914426+flashtyler@users.noreply.github.com>
2021-06-01 07:43:08 +0100

    Added todo list Update function.

--- todo/models.py
+++ todo/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
-class TodoList(models.Model): #Todolist able name that inherits models.Model
-    title = models.CharField(max_length=250) # a varchar
-    content = models.TextField(blank=True) # a text field 
-    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+
+# Todolist table name that inherits models.Model
+class TodoList(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     #location = models.ForeignKey(Location, default="general", on_delete=models.SET_DEFAULT) # a foreignkey
     location = models.CharField(max_length=100) # a varchar
+
     class Meta:
-        ordering = ["-created"] #ordering by the created field
+        # order by the 'created' field.
+        ordering = ["-created"]
+
     def __str__(self):
-        return self.title #name to be shown when called
+        # name to be shown when called.
+        return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,23 +1,52 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import TodoList
 
-def index(request): #the index view
-    todos = TodoList.objects.all() #quering all todos with the object manager
 
-    if request.method == "POST": #checking if the request method is a POST
-        if "taskAdd" in request.POST: #checking if there is a request to add a todo
+def index(request):
+    # Query all todolist items with the object manager.
+    todos = TodoList.objects.all()
+
+    # Check if the request method is a POST.
+    if request.method == "POST":
+        # Check if there is a request to add a a new todolist.
+        if "taskAdd" in request.POST:
+            title = request.POST["description"]
+            date = str(request.POST["date"])
+            location = request.POST["location_select"]
+            content = title + " -- " + date + " " + location
+            Todo = TodoList(title=title, content=content, due_date=date, location=location)
+            # Save the new todolist.
+            Todo.save()
+            # Reload the page.
+            return redirect("/")
+
+        # Check if there is a request to update an existing todolist.
+        if "taskUpdate" in request.POST:
+            # Get a list of all selected todolist items to be updated.
+            checkedlist = request.POST["checkedbox"]
+            for todo_id in checkedlist:
+                # Fetch the todolist.
+                todo = TodoList.objects.get(id=int(todo_id))
+                # Delete the todolist.
+                todo.delete()
+
             title = request.POST["description"] #title
             date = str(request.POST["date"]) #date
             location = request.POST["location_select"] # Location
             content = title + " -- " + date + " " + location # content
             Todo = TodoList(title=title, content=content, due_date=date, location=location)
-            Todo.save() #saving the todo 
-            return redirect("/") #reloading the page
+            Todo.update() #update the todo
+            # Reload the page.
+            return redirect("/")
 
-        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+        # Check if there is a request to delete an existing todolist.
+        if "taskDelete" in request.POST:
+            # Get a list of all selected todolist items to be updated.
+            checkedlist = request.POST["checkedbox"]
+
             for todo_id in checkedlist:
-                todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
-                todo.delete() #deleting todo
+                todo = TodoList.objects.get(id=int(todo_id))
+                # Delete the todolist.
+                todo.delete()
 
     return render(request, "index.html", {"todos": todos})
